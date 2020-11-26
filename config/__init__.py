2ced0b048fd926b0bf0948de9724a72983361b90
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2020-11-26 16:59:03 +0100

    more pathing stuff

--- config/__init__.py
+++ config/__init__.py
@@ -1,10 +1,17 @@
 SEQ_LEN = 256
 BATCH_SIZE = 64
 
+# Tokenizer parameters
+VOCAB_SIZE = 8192
+PADDING_SIZE = 256
 
 # Define paths
-PATH_TOKENIZER 	= "./data/serialized_tokenizer"
-PATH_TRAIN 		= "./data/tokenized_train"
-PATH_VAL 		= "./data/tokenized_val"
-PATH_TEST 		=	"./data/tokenized_test"
+PATH_DATA		= "./data/"
+PATH_TOKENIZER 	= PATH_DATA + "serialized_tokenizer"
+PATH_VOCAB 		= PATH_DATA + "vocab.json"
+PATH_MERGES 	= PATH_DATA + "merges.txt"
+PATH_TRAIN_TOK	= PATH_DATA + "tokenized_train"
+PATH_VAL_TOK 	= PATH_DATA + "tokenized_val"
+PATH_TEST_TOK 	= PATH_DATA + "tokenized_test"
+PATH_DATA_FILES = [PATH_DATA + f"wiki.{split}.raw" for split in ["test", "train", "valid"]]
 PATH_MODEL 		= "saved_model"
