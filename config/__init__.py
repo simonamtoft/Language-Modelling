9cf6f124044c0fd23255809a35b7c6dfe829991e
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2020-11-26 16:49:52 +0100

    path refactoring

--- config/__init__.py
+++ config/__init__.py
@@ -1,2 +1,10 @@
 SEQ_LEN = 256
 BATCH_SIZE = 64
+
+
+# Define paths
+PATH_TOKENIZER 	= "./data/serialized_tokenizer"
+PATH_TRAIN 		= "./data/tokenized_train"
+PATH_VAL 		= "./data/tokenized_val"
+PATH_TEST 		=	"./data/tokenized_test"
+PATH_MODEL 		= "saved_model"
