caff5ba6936900a653186118696ec288078b83a0
MarcMarabou <17163097+MarcMarabou@users.noreply.github.com>
2020-12-06 19:09:25 +0100

    Change to using DataLoader for batches to save memory.

--- TrainHelpers.py
+++ TrainHelpers.py
@@ -1,18 +1,20 @@
 import numpy as np
 import torch
+from torch.utils.data import DataLoader
 
-def prep_batches(dataset, batch_size, print_every = None):
-	num_batches = len(dataset) // batch_size
+def prep_batches(dataset, batch_size, print_every = None, num_workers=4):
+	data_iter = iter(DataLoader(dataset, batch_size=batch_size, drop_last=True, num_workers=num_workers))
+	num_batches = len(data_iter) # len(dataset) // batch_size
 	inputs =  [None] * num_batches
 	targets = [None] * num_batches
 
 	for batch in range(0, num_batches):
-		inputs[batch] = torch.stack(dataset[batch*batch_size : (batch+1)*batch_size])
-		targets[batch] = torch.zeros_like(inputs[batch])
+		ids = next(data_iter)["ids"]
+		inputs[batch] = ids #torch.stack(dataset[batch*batch_size : (batch+1)*batch_size])
+		targets[batch] = ids.clone()
 
 		for sequence in range(0, batch_size):
-			targets[batch][sequence][:-1] = inputs[batch][sequence][1:].detach().clone()
-			targets[batch][sequence][-1] = inputs[batch][sequence][0].detach().clone()
+			targets[batch][sequence] = torch.roll(targets[batch][sequence], -1, 0)
 		if print_every and batch % print_every == 0:
 			print("Preparing batch {}/{}".format(batch + 1, num_batches))
 	return inputs, targets
